08becfeea8de27759da7f9e74b9cc79b6449b769
NgocNhan <trantnnhan0403@gmail.com>
2023-04-15 08:59:49 +0700

    modify news_crawler.py

--- News_Crawler/news_crawler.py
+++ News_Crawler/news_crawler.py
@@ -1,11 +1,5 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
-import requests
-from bs4 import BeautifulSoup
-import csv
+from requests_html import HTMLSession
+import time
 import re
 
 def no_accent_vietnamese(s):
@@ -36,7 +30,25 @@ def jaccard_similarity(str1, str2):
     union = set1.union(set2)
     return len(intersection) / len(union)
 
-def crawl_articles(category, nums, startPage=1, recursion=0):
+def get_comments(link):
+    all_comments = []
+    try:
+        session = HTMLSession()
+        r = session.get(link)
+        r.html.render(sleep=1, timeout=30)
+        comments = r.html.find('.full_content')
+        accs = r.html.find('.txt-name')
+        for i in range(0, len(comments)):
+            accs[i]=accs[i].text
+            len_acc = len(accs[i])
+            comments[i] = comments[i].text
+            comments[i] = comments[i][len_acc:]
+            all_comments.append(comments[i])
+    except:
+        pass
+    return all_comments
+
+def crawl_articles(category, nums, startPage=1):
     categories =['thoi su', 'goc nhin', 'the gioi', 'kinh doanh', 'khoa hoc', 'giai tri', 'the thao', 'phap luat',
              'giao duc', 'suc khoe', 'doi song', 'du lich']
     max_ratio = max([jaccard_similarity(item, category) for item in categories])
@@ -44,46 +56,24 @@ def crawl_articles(category, nums, startPage=1, recursion=0):
         ratio = jaccard_similarity(item, category)
         if ratio == max_ratio:
             category = item
-    # Navigate to the category page
     url = 'https://vnexpress.net/'
     search_url = url + category.replace(' ', '-') + '-p' + str(startPage)
-    # Tải trang và sử dụng BeautifulSoup để phân tích cú pháp HTML
-    try:
-        page = requests.get(search_url)
-        page.raise_for_status()
-        soup = BeautifulSoup(page.content, 'html.parser')
-    except requests.exceptions.HTTPError as e:
-        print(f"HTTP error occurred: {e}")
-        return []
-
-    # Tìm các thẻ HTML chứa thông tin về bài báo
-    article_titles = soup.find_all('h3', {'class': 'title-news'})
-    # Duyệt qua các thẻ và trích xuất thông tin về bài báo
+    session = HTMLSession()
+    r = session.get(search_url)
+    r.html.render(sleep=1, timeout=30)
+    article_titles = r.html.find('.title-news')
     articles = []
     for item in article_titles:
-        title = item.find("a").get("title")
-        link = item.find("a").get("href")
-        driver = webdriver.Chrome() 
-        driver.get(link)
-        try:
-            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "content-comment")))
-        except TimeoutException:
-            print(f"Timed out waiting for page to load: {link}")
-            continue
-        comment_elements = driver.find_elements(By.CLASS_NAME, "full_content")
-        comments = []
-        for comment in comment_elements:
-            cmt = comment.text
-            comments.append(cmt)
-        # Append the article to the list
-        articles.append({'title': title, 'link': link, 'comment': comments})        
-        if len(articles) >= nums or recursion >= 10 :
-            return articles[:nums]
-        
-    # Nếu chưa đủ, tiếp tục đệ quy tới trang tiếp theo
-    if len(articles) < nums and recursion < 10 :
-        return articles + crawl_articles(category, nums - len(articles), startPage+1, recursion+1)
-        
+        title = item.find("a")[0].text
+        link = item.find("a")[0].attrs['href']
+        comments = get_comments(link)
+        articles.append({'title': title, 'link': link, 'comments': comments})
+        if len(articles) >= nums:
+            return articles
+    if len(articles) >= nums:
+            return articles
+    else:
+        return articles + crawl_articles(category, nums - len(articles), startPage+1)
 
-articles = crawl_articles('thời sự',2)
+articles = crawl_articles('Du lịch',1)
 print(articles)
